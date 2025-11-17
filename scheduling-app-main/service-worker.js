5472937c7bd32956434fc6939e723388b8740d33
Rakh90 <tripl3me@gmail.com>
2025-11-16 18:52:43 -0800

    Initial commit - Scheduling App

---
+++ scheduling-app-main/service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'scheduling-app-v1';
+const urlsToCache = [
+  './',
+  './scheduling-app.html',
+  './manifest.json',
+  './appiconschedule.png',
+  './appiconschedule2.png'
+];
+
+// Install event - cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
